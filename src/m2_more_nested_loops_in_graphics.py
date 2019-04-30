352574a3524bce6c0e66b9e3caad6f61960648a5
krsekjp <krsekjp@rose-hulman.edu>
2019-04-30 00:07:36 -0400

    DONE

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -4,8 +4,8 @@ in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Josh Krsek.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -50,9 +50,20 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # -------------------------------------------------------------------------
+    for k in range(n):
+        up_left1 = rg.Point(rectangle.get_upper_left_corner().x-(rectangle.get_width()/2)*k, rectangle.get_upper_left_corner().y-(rectangle.get_height())*k)
+        low_right1 = rg.Point(rectangle.get_lower_right_corner().x-(rectangle.get_width()/2)*k, rectangle.get_lower_right_corner().y-(rectangle.get_height())*k)
+        rect_1 = rg.Rectangle(up_left1,low_right1)
+        rect_1.attach_to(window)
+        for j in range(k):
+            up_left2 = rg.Point(up_left1.x+rect_1.get_width()*(j+1), up_left1.y)
+            low_right2 = rg.Point(low_right1.x+rect_1.get_width()*(j+1), low_right1.y)
+            rect_2 = rg.Rectangle(up_left2,low_right2)
+            rect_2.attach_to(window)
+    window.render()
 
 
 # -----------------------------------------------------------------------------
